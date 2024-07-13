408f2d34b46ddfab185ddb4d04ed9a9db03dfa6b
Daniel Drave <danieldrave@gmail.com>
2024-07-13 15:34:07 +0100

    Grandslam archive - Client - Search functionality and UI

--- client/index.js
+++ client/index.js
@@ -4,21 +4,36 @@ fetch(source)
     .then(response => response.json())
     .then(data => {
     grandslams = data;
-    data.forEach((slam) => {
-        Object.keys(slam)
-            .forEach(record => {
-            console.log(`${record} - ${slam[record]}`);
-        });
+    render(data);
+    document.querySelector(`input`)
+        .addEventListener(`input`, e => {
+        const target = e.target;
+        filter(target.value);
     });
 });
 /**
  * Renders table of information
+ * @param {Array<Object>} data - the results data
  * @returns {void}
  */
-const render = () => {
+const render = (data) => {
+    const el = document.querySelector(`#app tbody`);
+    let dataset = ``;
+    data.forEach((slam) => {
+        dataset += `<tr>`;
+        Object.keys(slam)
+            .forEach(record => dataset += `<td> ${slam[record]} </td>`);
+        dataset += `</tr>`;
+    });
+    el.innerHTML = dataset;
 };
 /**
+ * Filters the global data by input event
+ * @param {String} e - the target value entered on input
  * @returns {void}
  */
-const filter = () => {
+const filter = (e) => {
+    const query = grandslams.filter(slam => Object.keys(slam)
+        .some(record => slam[record].toLowerCase().includes(e.toLowerCase())));
+    render(query);
 };
